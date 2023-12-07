bef81ec096129a2d1c0b7b2a2dec553797eca733
William Hearn <williamhearn17@gmail.com>
2023-12-07 15:13:03 +0000

    Add commenting and rename variables

--- Modules/Microsoft365/Collection/AuthenticationLogs/Get-TenantAuthenticationLogs.ps1
+++ Modules/Microsoft365/Collection/AuthenticationLogs/Get-TenantAuthenticationLogs.ps1
@@ -1,60 +1,74 @@
 param (
+    # Start date and time for audit log search
     [Parameter(Mandatory = $true)]
-    [DateTime]$start,
+    [DateTime]$searchStartDate,
 
+    # End date and time for audit log search
     [Parameter(Mandatory = $true)]
-    [DateTime]$end,
+    [DateTime]$searchEndDate,
 
+    # Default output path for saving the audit logs
     [Parameter(Mandatory = $true)]
-    [string]$outputPath = "C:\temp\"
+    [string]$auditLogOutputPath = "C:\temp\"
 )
 
-$operationType = "UserLoggedIn"
-$sessionID = New-SessionID
-$resultSize = 5000
+# Specifies the type of operation to search in audit logs
+$auditOperationType = "UserLoggedIn"
 
-function New-SessionID() {
+# Generate a unique session ID for each search
+$auditSessionID = New-AuditSessionID
+
+# Sets the maximum number of records to retrieve in a single search
+$maximumResultSize = 5000
+
+# Generates a new session ID for audit log search
+function New-AuditSessionID() {
     return [Guid]::NewGuid().ToString() + "_" + "ExtractLogs" + (Get-Date).ToString("yyyyMMddHHmmssfff")
 }
 
-function Search-AuditLog($start, $end, $sessionID, $resultSize, $outputPath, $operationType) {
-    $maxRetries = 3
-    $retryCount = 0
+# Searches the Unified Audit Log based on provided parameters
+function Search-AuditLog($startDate, $endDate, $sessionID, $resultSize, $outputPath, $operationType) {
+    $maximumRetryAttempts = 3
+    $currentRetryCount = 0
 
-    while ($retryCount -lt $maxRetries) {
+    while ($currentRetryCount -lt $maximumRetryAttempts) {
         try {
-            $results = Search-UnifiedAuditLog -StartDate $start -EndDate $end -SessionId $sessionID -SessionCommand ReturnLargeSet -ResultSize $resultSize -RecordType $operationType
+            # Perform the audit log search
+            $auditResults = Search-UnifiedAuditLog -StartDate $startDate -EndDate $endDate -SessionId $sessionID -SessionCommand ReturnLargeSet -ResultSize $resultSize -RecordType $operationType
 
-            if ($results.Count -gt 0) {
-                $results | Export-Csv -Path "$($outputPath)\AuditLogs.csv" -Append -NoTypeInformation -Encoding UTF8
+            # Export results to CSV if any records are found
+            if ($auditResults.Count -gt 0) {
+                $auditResults | Export-Csv -Path "$($outputPath)\AuditLogs.csv" -Append -NoTypeInformation -Encoding UTF8
             }
 
-            return $results.Count -gt 0
+            return $auditResults.Count -gt 0
         }
         catch {
             Write-Host "Error encountered: $($_.Exception.Message). Retrying..." -ForegroundColor Yellow
             Start-Sleep -Seconds 10
-            $retryCount++
+            $currentRetryCount++
         }
     }
-    Write-Host "Error after $maxRetries retries. Continuing to next batch..." -ForegroundColor Red
+    Write-Host "Error after $maximumRetryAttempts retries. Continuing to next batch..." -ForegroundColor Red
 }
 
-function Get-AllAuditRecords($start, $end, $operationType, $outputPath) {
-    $currentStart = $start
-    $intervalDays = 1 # Process one day at a time, adjust as needed
+# Retrieves all audit records within the specified date range
+function Get-AllAuditRecords($startDate, $endDate, $operationType, $outputPath) {
+    $currentStartDate = $startDate
+    $processingIntervalDays = 1 # Process one day at a time, adjust as needed
 
-    while ($currentStart -lt $end) {
-        $currentEnd = $currentStart.AddDays($intervalDays)
-        if ($currentEnd -gt $end) { $currentEnd = $end }
+    while ($currentStartDate -lt $endDate) {
+        $currentEndDate = $currentStartDate.AddDays($processingIntervalDays)
+        if ($currentEndDate -gt $endDate) { $currentEndDate = $endDate }
 
-        $hasResults = Search-AuditLog $currentStart $currentEnd $sessionID $resultSize $outputPath $operationType
+        # Search for audit logs within the specified interval
+        $hasResults = Search-AuditLog $currentStartDate $currentEndDate $sessionID $resultSize $outputPath $operationType
 
         if (!$hasResults) { break }
 
-        $currentStart = $currentEnd
+        $currentStartDate = $currentEndDate
     }
 }
 
-
-Get-AllAuditRecords $start $end $operationType $outputPath
+# Start the audit log retrieval process
+Get-AllAuditRecords $searchStartDate $searchEndDate $auditOperationType $auditLogOutputPath
