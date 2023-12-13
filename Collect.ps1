53e43e26180c822040c0b21fde83dbb3a57241a7
William Hearn <williamhearn17@gmail.com>
2023-12-13 01:32:27 +0000

    Add logging statements Add export csv to mailbox audit logs Add tenant domain name to file path

--- Collect.ps1
+++ Collect.ps1
@@ -1,11 +1,41 @@
 .\Modules\Reload-Modules.ps1
 Import-Module .\Modules\Logging\Logging.psm1
-Import-Module .\Modules\Microsoft365\Authentication\M365Auth.ps1
+Import-Module .\Modules\Microsoft365\Authentication\M365Auth.psm1
+Import-Module .\Modules\Microsoft365\Collection\MailboxAuditLogs\Get-MailboxAuditLogs.psm1
+
+function Get-AllMailboxAuditLogsSubroutine {
+    param (
+        [string[]]$UserPrincipalNames,
+        [string]$DomainName
+    )
+
+    $ExchangeMailboxAuditLogs = @()
+
+    foreach ($UserPrincipalName in $UserPrincipalNames) {
+        $ExchangeMailboxAuditLogs += Get-AllMailboxAuditLogs -UserPrincipalName $UserPrincipalName -StartDate -EndDate # FILL THIS OUT
+    }
+
+    $ExchangeMailboxAuditLogs | Export-Csv -Path "$(Get-Location)\$($DomainName)" -NoTypeInformation
+}
 
 function main {
 
+    Write-ConsoleLog -Message "Collector started" -Level "info"
+    Out-FileLog -Message "Collector started" -Level "info"
+
     Write-ConsoleLog -Message "Connecting to M365 Endpoints" -Level "info"
+    Out-FileLog -Message "Connecting to M365 Endpoints" -Level "info"
     Connect-M365
+
+    [string]$TenantDomainName = (Get-AcceptedDomain | Where-Object { $_.Default -eq $true }).DomainName
+
+    Write-ConsoleLog -Message "Gathering user principal name list from tenant" -Level "info"
+    Out-FileLog -Message "Gathering user principal name list from tenant" -Level "info"
+    [string[]]$AllUserPrincipalNames = Get-Mailbox -ResultSize Unlimited | Select-Object UserPrincipalName
+
+    Write-ConsoleLog -Message "Gathering user mailbox audit logs" -Level "info"
+    Out-FileLog -Message "Gathering user mailbox audit logs" -Level "info"
+    Get-AllMailboxAuditLogsSubroutine -UserPrincipalNames $AllUserPrincipalNames -DomainName $TenantDomainName
 }
 
 main
