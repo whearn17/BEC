588cb063c7f74194c9a9f0f26234cdfc24a7d723
William Hearn <williamhearn17@gmail.com>
2024-01-08 17:17:45 +0000

    Add routines for user auth logs Add routines for user inbox rules Fix imports

--- Collect.ps1
+++ Collect.ps1
@@ -1,4 +1,4 @@
-.\Modules\Reload-Modules.ps1
+.\Modules\ReloadModules.ps1
 Import-Module .\Modules\Logging\Logging.psm1
 Import-Module .\Modules\Microsoft365\Authentication\M365Auth.psm1
 Import-Module .\Modules\Microsoft365\Collection\MailboxAuditLogs\MailboxAuditLogs.psm1
@@ -19,9 +19,25 @@ function main {
     Out-FileLog -Message "Gathering user principal name list from tenant" -Level "info"
     [string[]]$AllUserPrincipalNames = Get-Mailbox -ResultSize Unlimited | Select-Object UserPrincipalName
 
+
+    # Exchange Mailbox Audit Logs
+
     Write-ConsoleLog -Message "Gathering user mailbox audit logs" -Level "info"
     Out-FileLog -Message "Gathering user mailbox audit logs" -Level "info"
-    Get-MailboxAuditLogs -UserPrincipalNames $AllUserPrincipalNames -DomainName $TenantDomainName
+    Get-MailboxAuditLogs -UserPrincipalNames $AllUserPrincipalNames -DomainName $TenantDomainName -StartDate (Get-Date).AddDays(-90) -EndDate Get-Date
+
+    # Unified Audit Logs
+
+    $AuthenticationEvents = @("UserLoggedIn")
+    $InboxRuleEvents = @("New-InboxRule", "Set-InboxRule", "Remove-InboxRule")
+
+    Write-ConsoleLog -Message "Gathering user authentication logs" -Level "info"
+    Out-FileLog -Message "Gathering user authentication logs" -Level "info"
+    Get-AuditLogs -SearchStartDate (Get-Date).AddDays(-90) -EndDate Get-Date -EventTypes $AuthenticationEvents
+
+    Write-ConsoleLog -Message "Gathering user inbox rule logs" -Level "info"
+    Out-FileLog -Message "Gathering user inbox rule logs" -Level "info"
+    Get-AuditLogs -SearchStartDate (Get-Date).AddDays(-90) -EndDate Get-Date -EventTypes $InboxRuleEvents
 }
 
 main
