0c82c3d527dcdb21663a47f17ccbeac8a1ef89e6
William Hearn <williamhearn17@gmail.com>
2024-01-09 15:29:13 +0000

    Add log file output paths to Get-AuditLogs function calls Add function New-RequiredDirectories which creates all necesssary directories for output

--- M365Collector.ps1
+++ M365Collector.ps1
@@ -5,11 +5,27 @@ Import-Module .\Modules\Microsoft365\Collection\MailboxAuditLogs\MailboxAuditLog
 Import-Module .\Modules\Microsoft365\Collection\AuditLogs\AuditLogs.psm1
 
 $LogFile = ".\Logs\archimedes$(Get-Date -Format "yyyy-MM-dd").log"
+$InboxRulesLogOutput = ".\Output\Logs\InboxRules\"
+$AuthenticationLogOutput = ".\Output\Logs\Authentication"
+
+function New-RequiredDirectories {
+    [string[]] $RequiredDirectories = @(".\Logs", ".\Output", ".\Output\Logs", ".\Output\Logs\InboxRules\", ".\Output\Logs\Authentication")
+
+    foreach ($Directory in $RequiredDirectories) {
+        if (-not (Test-Path $Directory)) {
+            New-Item -Path $Directory -ItemType "directory"
+        }
+    }
+}
 
 function main {
 
     Write-ConsoleLog -Message "Collector started" -Level "info"
     Out-FileLog -Message "Collector started" -Level "info" -LogPath $LogFile
+
+    Write-ConsoleLog -Message "Creating output directories" -Level "info"
+    Out-FileLog -Message "Creating output directories" -Level "info"
+    New-RequiredDirectories
 
     Write-ConsoleLog -Message "Connecting to M365 Endpoints" -Level "info"
     Out-FileLog -Message "Connecting to M365 Endpoints" -Level "info" -LogPath $LogFile
@@ -35,11 +51,11 @@ function main {
 
     Write-ConsoleLog -Message "Gathering user authentication logs" -Level "info"
     Out-FileLog -Message "Gathering user authentication logs" -Level "info" -LogPath $LogFile
-    Get-AuditLogs -SearchStartDate (Get-Date).AddDays(-90) -EndDate Get-Date -EventTypes $AuthenticationEvents
+    Get-AuditLogs -SearchStartDate (Get-Date).AddDays(-90) -EndDate Get-Date -EventTypes $AuthenticationEvents -AuditLogOutputPath $AuthenticationLogOutput
 
     Write-ConsoleLog -Message "Gathering user inbox rule logs" -Level "info"
     Out-FileLog -Message "Gathering user inbox rule logs" -Level "info" -LogPath $LogFile
-    Get-AuditLogs -SearchStartDate (Get-Date).AddDays(-90) -EndDate Get-Date -EventTypes $InboxRuleEvents
+    Get-AuditLogs -SearchStartDate (Get-Date).AddDays(-90) -EndDate Get-Date -EventTypes $InboxRuleEvents -AuditLogOutputPath $InboxRulesLogOutput
 }
 
 main
